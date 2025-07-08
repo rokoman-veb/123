b8079e40b406dd1bcd685984129807858062086e
rokoman-veb <lucifernakahara@gmail.com>
2025-07-08 16:41:18 +0300

    Add files via upload

---
+++ practice_5/practice_6/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int x1, y1, x2, y2;
+    scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
+
+    double len = sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1));
+    printf("%.6lf\n", len);
+
+    return 0;
+}
